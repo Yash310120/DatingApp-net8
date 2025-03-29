20f4c095022ce015fb89dc6259998888097939fb
Yash310120 <yashgatagat202020@gmail.com>
2025-03-29 16:08:45 +0530

    end of section 2

---
+++ entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DATINGAPP.entities
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Username { get; set; } 
+    }
+}
